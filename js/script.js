e2e13a8c5be69e4b46d16a9af68002fa85b44d30
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2024-09-02 10:22:49 +0200

    corretto errori

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log('Js Cololegato')
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            newTodo: '',
+            todos: [      // Lista di todo con oggetti
+                { text: 'Imparare Vue.js', done: false },
+                { text: 'Studiare Javascript', done: false },
+                { text: 'Posare corda e sgabello', done: true }
+            ]
+        };
+    },
+    methods: {
+        addTodo() {
+            // Verifica se il campo di input non è vuoto
+            if (this.newTodo.trim() !== '') {
+                // Aggiunge un nuovo todo all'array todos
+                this.todos.push({ text: this.newTodo.trim(), done: false });
+                // Resetta il campo di input
+                this.newTodo = '';
+            }
+        },
+        removeTodo(index) {
+            // Rimuove il todo all'indice specificato
+            this.todos.splice(index, 1);
+        },
+        toggleDone(todo) {
+            // Inverte lo stato di completamento del todo
+            todo.done = !todo.done;
+        }
+    }
+}).mount('#app');
